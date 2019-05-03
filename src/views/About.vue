a4032090f2d9a183cf5edf1c00d1f56eab4b85fc
何建钦 <1373883296@qq.com>
2019-05-03 11:32:13 +0800

    init project

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <van-notice-bar text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"
+      left-icon="volume-o" />
+  </div>
+</template>
